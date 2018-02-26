57e1e172abd7030e5370bfb5c7c867829c3c7ce6
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2018-02-26 12:52:36 +0000

    started setting up the assets and code for a top down shooter game

---
+++ Top down shooter game/Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController2D : MonoBehaviour {
+
+    public float speed = 5.0f;
+    Rigidbody2D rigidBody2D;
+
+	void Start () {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rigidBody2D.velocity = new Vector2(x, y) * speed;
+        rigidBody2D.angularVelocity = 0.0f;
+	}
+}
