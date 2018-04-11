1a0d6871e92d29b610a206463dd6413144b92fcf
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2018-04-11 17:42:41 +0100

    made progress creating 1st level and started adding A* pathfinding

--- Escape From The Pit/Assets/Scripts/BulletHit2D.cs
+++ Escape From The Pit/Assets/Scripts/BulletHit2D.cs
@@ -6,14 +6,12 @@ public class BulletHit2D : MonoBehaviour {
 
     public int damage = 1;
     public string damageTag = "";
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(damageTag))
         {
             other.SendMessage("TakeDamage", damage);
         }
-
         Destroy(gameObject);
-    }
+    }
 }
---
+++ Escape From The Pit/Assets/Scripts/GuardBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardBehaviour : MonoBehaviour {
+
+    public int health = 10;
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
